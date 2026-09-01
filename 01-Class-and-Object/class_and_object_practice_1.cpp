409db5407d06c131bf3dd6420cb1aecf9476bd16
inan58621-codepractice <inan58621@gmail.com>
2026-09-01 19:56:46 +0600

    Create class_and_object_practice_1.cpp

---
+++ 01-Class-and-Object/class_and_object_practice_1.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+
+    int age ;
+};
+
+
+
+
+int main()
+{
+    Student S1;
+
+    S1.name = "Inan Mahmud";
+    S1.age = 21;
+
+    cout<<"Name : " <<S1.name<<endl;
+
+    cout<<"Age : "<<S1.age<<endl;
+
+
+
+
+
+
+    return 0;
+}
+
+
