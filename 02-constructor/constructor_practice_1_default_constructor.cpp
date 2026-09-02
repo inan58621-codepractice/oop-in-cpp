589a0f47049d03b5f5cf3e53e6e6a0661d7f296b
inan58621-codepractice <inan58621@gmail.com>
2026-09-02 17:54:12 +0600

    Create constructor_practice_1_default_constructor.cpp

---
+++ 02-constructor/constructor_practice_1_default_constructor.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+
+    Student()
+    {
+        cout<<"Student object created"<<endl;
+    }
+
+};
+
+int main()
+{
+    Student s1;
+}
